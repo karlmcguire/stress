73f26f75edb327a2df2d88c1fd0c11e328545622
Karl McGuire <karl@xba.io>
2019-11-03 23:50:28 -0500

    initial

---
+++ basic/go.mod
@@ -0,0 +1,3 @@
+module github.com/karlmcguire/stress/basic
+
+go 1.12
---
+++ stress_test.go
@@ -0,0 +1,58 @@
+package stress
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/karlmcguire/stress/basic"
+)
+
+const (
+	numKeys       = 1e7
+	keyMask       = numKeys - 1
+	numGoroutines = 8
+	segSize       = numKeys / numGoroutines
+)
+
+type (
+	Map interface {
+		Get(uint64) uint64
+		Set(uint64, uint64)
+		SetAll([][2]uint64)
+		Del(uint64)
+	}
+	Benchmark struct {
+		Name string
+		Map  Map
+	}
+)
+
+func genKeys() [numKeys]uint64 {
+	var keys [numKeys]uint64
+	for i := range keys {
+		keys[i] = rand.Uint64() % numKeys
+	}
+	return keys
+}
+
+func genBenchmarks() []*Benchmark {
+	return []*Benchmark{
+		{"basic", basic.New(numKeys)},
+	}
+}
+
+func BenchmarkMaps(b *testing.B) {
+	keys, benchmarks := genKeys(), genBenchmarks()
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.Name, func(b *testing.B) {
+			b.SetBytes(1)
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for i := rand.Int() & keyMask; pb.Next(); i++ {
+					benchmark.Map.Set(keys[i&keyMask], keys[i&keyMask])
+				}
+			})
+		})
+	}
+}
